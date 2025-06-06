c4d63a56be6ec81107bc0d892cf3b0dee3f0972e
vimbai murerwa <vimbiemurerwa@gmail.com>
2025-06-06 13:51:36 +0200

    updated login

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,14 @@
 import { initializeApp } from "firebase/app";
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
-import { addDoc, collection, getFirestore } from "firebase/firestore"
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
 import { toast } from "react-toastify";
 
-// web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyAkkMCpqErzQ8iYziBVPygTsZ2nkhkGVnk",
   authDomain: "netflix-clone-e2da3.firebaseapp.com",
@@ -13,42 +18,47 @@ const firebaseConfig = {
   appId: "1:141795954036:web:27d1f14caeee5233eef021"
 };
 
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
 // Sign-up function to create a new user with email and password
-const signup = async (name, email, password)=>{
-    try {
-     const res = await createUserWithEmailAndPassword(auth, email, password);
+const signup = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
     const user = res.user;
 
-     // Store the user's information in Firestore database
+    // Save user info to Firestore
     await addDoc(collection(db, "user"), {
-        uid: user.uid,
-        name,
-        authProvider: "local",
-        email,
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
     });
-    }catch (error) {
-      console.log(error);
-      toast.error(error.code.split('/')[1].split('-').join(" "));  // Display error message using react-toastify
-    }
-}
-
-const login = async (email, password)=>{
-  try {
-    signInWithEmailAndPassword(auth, email, password);
+    return user;
   } catch (error) {
-console.log(error);
-toast.error(error.code.split('/')[1].split('-').join(" "));
+    console.log(error);
+    toast.error(error.code.split('/')[1].split('-').join(" "));
+    throw error;
   }
-}
+};
 
-// Logout function to sign the user out
-const logout = ()=>{
+// Login function
+const login = async (email, password) => {
+  try {
+    const res = await signInWithEmailAndPassword(auth, email, password);
+    return res.user;
+  } catch (error) {
+    console.log(error);
+    toast.error(error.code.split('/')[1].split('-').join(" "));
+    throw error;
+  }
+};
+
+
+const logout = () => {
   signOut(auth);
-}
+};
 
-export {auth, db, login, signup, logout};
+export { auth, db, login, signup, logout };
